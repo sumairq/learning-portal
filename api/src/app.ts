04de6f2c63f4b30fdc2a0974711e3d0190d0e4ca
sumairq <sumairqaisar992@gmail.com>
2026-08-31 19:07:49 +0500

    Add database configuration

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,8 +1,9 @@
 // this is app.ts file
 import express from "express";
+import dbConnect from "./config/db.js";
 const app = express()
 
-
+dbConnect()
 
 app.get('/', (req,res)=>{
     res.send('Hello World')
--- api/src/config/db.ts
+++ api/src/config/db.ts
@@ -1,1 +1,32 @@
 // sample file
+
+import mongoose from "mongoose";
+
+
+const dbConnect = async ()=>{
+  try{
+    const mongoURI = process.env.MONGO_URI
+    if(!mongoURI){
+        console.error('❌ Error: MONGO_URI environment variable is missing.')
+        process.exit(1);
+    }
+    const conn =  await mongoose.connect(mongoURI)
+    console.log(`🍃 MongoDB Connected: ${conn.connection.host}`)
+
+  }catch(error){
+    console.error(`❌ Initial connection error: ${(error as Error).message}`)
+    process.exit(1);
+  }
+}
+
+mongoose.connection.on('error',(err)=>{
+    console.error(`🚨 MongoDB runtime error: ${err}`);
+})
+
+
+mongoose.connection.on('disconnected',()=>{
+    console.warn('⚠️ MongoDB connection lost. Attempting reconnection...');
+})
+
+
+export default dbConnect
